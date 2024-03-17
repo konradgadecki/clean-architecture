a4c8973ce4e07cda08ba5660af1fcf5d4eb54d2f
Konrad Gadecki <konradgadecki@gmail.com>
2024-03-17 12:28:07 +0100

    implemented repository pattern (#8)

--- src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -1,12 +1,28 @@
 using ErrorOr;
+using GymManagement.Application.Common.Interfaces;
 using GymManagement.Application.Subscriptions.Commands.CreateSubscription;
+using GymManagement.Domain.Subscriptions;
 using MediatR;
 
 public class CreateSubscriptionCommandHandler 
-: IRequestHandler<CreateSubscriptionCommand, ErrorOr<Guid>>
+: IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
 {
-    public async Task<ErrorOr<Guid>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+    private ISubscriptionsRepository _subscriptionsRepository;
+
+    public CreateSubscriptionCommandHandler(ISubscriptionsRepository subscriptionsRepository)
     {
-        return Guid.NewGuid();
+        _subscriptionsRepository = subscriptionsRepository;
+    }
+
+    public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+    {
+        var subscription = new Subscription()
+        {
+            Id = Guid.NewGuid()
+        };
+
+         _subscriptionsRepository.AddSubscription(subscription);
+
+         return subscription;
     }
 }
