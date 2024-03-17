c19becd9e4008644f0f95279811690634a677765
konradgadecki <konradgadecki@gmail.com>
2024-03-17 17:11:58 +0100

    provide in memory implementation of repository

--- src/GymManagement.Application/Common/Interfaces/ISubscriptionsRepository.cs
+++ src/GymManagement.Application/Common/Interfaces/ISubscriptionsRepository.cs
@@ -4,5 +4,6 @@ namespace GymManagement.Application.Common.Interfaces;
 
 public interface ISubscriptionsRepository
 {
-    Subscription AddSubscription(Subscription subscription);
+    Task AddSubscriptionAsync(Subscription subscription);
+    Task<Subscription?> GetByIdAsync(Guid subscriptionId);
 }
--- src/GymManagement.Infrastructure/DependencyInjection.cs
+++ src/GymManagement.Infrastructure/DependencyInjection.cs
@@ -1,3 +1,5 @@
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Infrastructure.Subscriptions.Persistence;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GymManagement.Infrastructure;
@@ -5,5 +7,5 @@ namespace GymManagement.Infrastructure;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services) =>
-        services;
+        services.AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
 }
