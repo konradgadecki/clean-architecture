95710e1c183ba037358cb013fca73079de1356dc
konradgadecki <konradgadecki@gmail.com>
2024-03-17 18:01:22 +0100

    added entity framework with sqlite database

--- src/GymManagement.Infrastructure/DependencyInjection.cs
+++ src/GymManagement.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using GymManagement.Application.Common.Interfaces;
+using GymManagement.Infrastructure.Common.Persistence;
 using GymManagement.Infrastructure.Subscriptions.Persistence;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GymManagement.Infrastructure;
@@ -7,5 +9,9 @@ namespace GymManagement.Infrastructure;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services) =>
-        services.AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
+        services
+        .AddDbContext<GymManagementDbContext>(options => {
+            options.UseSqlite("Data Source = GymManagement.db");
+        })
+        .AddScoped<ISubscriptionsRepository, SubscriptionsRepository>();
 }
