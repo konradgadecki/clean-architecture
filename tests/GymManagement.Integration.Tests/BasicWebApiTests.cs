3e59cfeb60340a4b51e55abfa1de94051ec21eb4
VOLUEAD\konrad.gadecki <konrad.gadecki@volue.com>
2024-03-21 19:03:18 +0100

    added basic integration tests for web api and database

---
+++ tests/GymManagement.Integration.Tests/BasicWebApiTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace GymManagement.Integration.Tests;
+
+public class BasicWebApiTests 
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public BasicWebApiTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Theory]
+    [InlineData("/UnitTest")]
+    public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+        Assert.Equal("text/plain; charset=utf-8", 
+            response.Content.Headers.ContentType.ToString());
+    }
+}
