8c704d98138859eb10beb9fb8af688854df41bb4
Konrad Gadecki <konradgadecki@gmail.com>
2024-06-15 21:37:23 +0200

    model validation in pipeline behavior (#17)

---
+++ src/GymManagement.Application/Gyms/Commands/CreateGym/CreateGymCommandBehavior.cs
@@ -0,0 +1,28 @@
+﻿using ErrorOr;
+using GymManagement.Domain.Gyms;
+using MediatR;
+
+namespace GymManagement.Application.Gyms.Commands.CreateGym;
+
+public class CreateGymCommandBehavior : IPipelineBehavior<CreateGymCommand, ErrorOr<Gym>>
+{
+    public async Task<ErrorOr<Gym>> Handle(
+        CreateGymCommand request,
+        RequestHandlerDelegate<ErrorOr<Gym>> next,
+        CancellationToken cancellationToken
+    )
+    {
+        var validator = new CreateGymCommandValidator();
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (validationResult.IsValid is false)
+        {
+            return validationResult
+                .Errors.Select(error =>
+                    Error.Validation(code: error.PropertyName, description: error.ErrorMessage)
+                )
+                .ToList();
+        }
+
+        return await next();
+    }
+}
---
+++ src/GymManagement.Application/Gyms/Commands/CreateGym/CreateGymCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace GymManagement.Application.Gyms.Commands.CreateGym;
+
+public class CreateGymCommandValidator : AbstractValidator<CreateGymCommand>
+{
+    public CreateGymCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(3).WithMessage("Name must exceed 3 characters.")
+            .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+    }
+}
