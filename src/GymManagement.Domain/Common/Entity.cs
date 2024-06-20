4332a9268af9d479946ec36a70a6e6a837ca4c67
Konrad Gadecki <konradgadecki@gmail.com>
2024-06-20 20:25:06 +0200

    added domain events (#19)

---
+++ src/GymManagement.Domain/Common/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace GymManagement.Domain.Common;
+
+public abstract class Entity
+{
+    public Guid Id { get; init; }
+
+    protected readonly List<IDomainEvent> _domainEvents = new();
+
+    protected Entity(Guid id) => this.Id = id;
+
+    protected Entity() { }
+}
